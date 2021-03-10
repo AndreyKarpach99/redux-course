545fd4e6c7f58f19b17dfe16b316457a176e0bcb
Andreykas01 <andreykarpach99@gmail.com>
2021-03-10 13:15:32 +0300

    update 10.03.2021 13-15

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import './styles.css'
-import {createStore} from './createStore'
+import {createStore, applyMiddleware} from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import { increment, decrement, asyncIncrement, changeTheme } from './redux/actions'
 import {rootReducer} from './redux/rootReducer'
+import './styles.css'
 
 const counter = document.getElementById('counter')
 const addBtn = document.getElementById('add')
@@ -8,23 +12,58 @@ const subBtn = document.getElementById('sub')
 const asyncBtn = document.getElementById('async')
 const themeBtn = document.getElementById('theme')
 
-const store = createStore(rootReducer, 0)
+// как работает Middleware пример
+
+// function logger(state) {
+//   return function(next) {
+//     return function(action) {
+//       console.log('Prev state', state.getState())
+//       console.log('Action', action)
+
+//       const newState = next(action);
+//       console.log('New state', newState)
+//       return newState
+//     }
+//   }
+// }
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk, logger)
+  )
+)
 
 addBtn.addEventListener('click', () => {
-
+  store.dispatch(increment())
 })
 
 subBtn.addEventListener('click', () => {
-
+  store.dispatch(decrement())
 })
 
 asyncBtn.addEventListener('click', () => {
+  store.dispatch(asyncIncrement())
 
 })
 
 themeBtn.addEventListener('click', () => {
-  // document.body.classList.toggle('dark')
+  const newTheme = document.body.classList.contains('light')
+  ? 'dark'
+  : 'light'
+
+  store.dispatch(changeTheme(newTheme))
 })
 
+store.subscribe(() => {
+  const state = store.getState()
 
-render()
+  counter.textContent = state.counter
+  document.body.className = state.theme.value;
+
+  [addBtn, subBtn, themeBtn, asyncBtn].forEach((btn) => {
+    btn.disabled = state.theme.disabled
+  })
+})
+
+store.dispatch({type: 'INIT_APPLICATION'})
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,48 @@
-export function rootReducer(state, action) {
+import { combineReducers } from 'redux'
+import {INCREMENT, DECREMENT, CHANGE_THEME, ENABLE_BUTTONS, DISABLE_BUTTONS} from './types'
 
-  if(action.type === 'INCREMENT') {
+function counterReducer(state = 0, action) {
+
+  if(action.type === INCREMENT) {
     return state + 1
-  } else if(action.type === 'DECREMENT') {
+  } else if(action.type === DECREMENT) {
     return state - 1
-  } 
+  }
 
   return state
 }
+
+const initialThemeState = {
+  value: 'light',
+  disabled: false
+}
+
+function themeReducer(state = initialThemeState, action) {
+
+  switch(action.type) {
+    case CHANGE_THEME:
+      return {
+        ...state,
+        value: action.payload
+      }
+    case ENABLE_BUTTONS:
+      return {
+        ...state,
+        disabled: false
+      }
+    case DISABLE_BUTTONS:
+      return {
+        ...state,
+        disabled: true
+      }
+    default: 
+      return state
+  }
+}
+
+// combineReducers предназначена для того чтобы комбинировать несколько reducers в один
+
+export const rootReducer = combineReducers({
+  counter: counterReducer,
+  theme: themeReducer
+})
